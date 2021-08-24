c111a090eef0ef4dfa50fa777470895614ab17c2
freemonic <luckmusk@yahoo.com>
2021-08-25 00:21:36 +0600

    Edit blog/views.py. Edit blog/urls.py. Edit post/list.htm.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 1) # По 3 статьи на каждой странице.
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Если страница не является целым числом, возвращаем первую страницу.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 1) # По 3 статьи на каждой странице.
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # Если страница не является целым числом, возвращаем первую страницу.
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
